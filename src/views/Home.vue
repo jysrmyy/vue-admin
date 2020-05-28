f19867f931b0f4bd01a26de2abd24ceb63a5a2db
liuxiao <liuxiao2100@foxmail.com>
2020-05-28 23:06:47 +0800

    完成学时3内容

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="嘤嘤嘤~Welcome to Your Vue.js App"/>
   </div>
 </template>
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,19 @@
+<template>
+
+<div id="login">
+hshshsh
+<el-button type="primary">主要按钮</el-button>
+</div>
+
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+#login{
+    height: 50vh;
+    background-color: #344a5f;   
+}
+</style>
